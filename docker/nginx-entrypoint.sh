62b7011355f0867bfca0b719870d799f80816112
xaturno-coder <xaturnowebservices@gmail.com>
2025-09-12 17:30:19 -0600

    add 1

--- docker/nginx-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Substitute environment variables into a new file
-envsubst '$DOMAIN' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.generated.conf
-
-# Start Nginx using the generated config
-nginx -g "daemon off;" -c /etc/nginx/conf.d/default.generated.conf
