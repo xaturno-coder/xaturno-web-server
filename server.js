261bb4a4c8c863c259679443256a1d215cd9d77b
xaturno-coder <xaturnowebservices@gmail.com>
2025-09-12 19:15:02 -0600

    reset

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("XATURNO.MX 2025");
+  res.send("Hello from my Dockerized Web Server 🚀");
 });
 
 app.listen(3000, () => {
