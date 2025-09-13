9c35f52a8074a8634098af38fe94f9352570d3c6
xaturno-coder <xaturnowebservices@gmail.com>
2025-09-12 20:46:00 -0600

    reset

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from my Dockerized Web Server 🚀");
-});
+const asciiBanner = `
+#     #    #    ####### #     # ######  #     # #######     #####    ###    #####  ####### 
+ #   #    # #      #    #     # #     # ##    # #     #    #     #  #   #  #     # #       
+  # #    #   #     #    #     # #     # # #   # #     #          # #     #       # #       
+   #    #     #    #    #     # ######  #  #  # #     #     #####  #     #  #####  ######  
+  # #   #######    #    #     # #   #   #   # # #     #    #       #     # #             # 
+ #   #  #     #    #    #     # #    #  #    ## #     #    #        #   #  #       #     # 
+#     # #     #    #     #####  #     # #     # #######    #######   ###   #######  #####  
+`;
 
+app.get("/", (req, res) => {
+  // Enviamos como texto plano para que se vea bien en monoespaciado
+  res.setHeader("Content-Type", "text/plain");
+  res.send(asciiBanner);
+});
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
